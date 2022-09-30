82f1121b8f05b828ba175d24a6032526f782b767
Sevara Yuldasheva <sevara878787@mail.ru>
2022-09-30 15:38:49 -0400

    alert class

---
+++ src/test/java/com/cydeo/test/day6_alerts_iframe_windows/Task2_AlertPractices.java
@@ -0,0 +1,50 @@
+package com.cydeo.test.day6_alerts_iframe_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task2_AlertPractices {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        //1. Open browser
+        //2. Go to website: http://practice.cydeo.com/javascript_alerts
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alertTask1(){
+
+        //3. Click to “Click for JS Alert” button
+        WebElement informationAlertBtn = driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+        informationAlertBtn.click();
+
+        //4. Click to OK button from the alert
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        //5. Verify “You successfully clicked an alert” text is displayed
+        String actualSuccessMessage = driver.findElement(By.xpath("//p[@id='result']")).getText();
+        String expectedSuccessMessage = "You successfully clicked an alert";
+
+        Assert.assertEquals(actualSuccessMessage, expectedSuccessMessage);
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
